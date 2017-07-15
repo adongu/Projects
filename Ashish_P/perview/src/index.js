02e8369e9ef647e88147afe2173426420ea22613
AlexLiang <liangalex91@gmail.com>
2017-07-15 00:36:07 -0400

    Fix hot reloading not working due to App.js file directory change

--- Ashish_P/perview/src/index.js
+++ Ashish_P/perview/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './containers/App.js';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 if(module.hot) {
-  module.hot.accept('./App', () => {
-    const NextApp = require('./App').default
+  module.hot.accept('./containers/App.js', () => {
+    const NextApp = require('./containers/App.js').default
     ReactDOM.render(
       <NextApp />,
       document.getElementById('root')
