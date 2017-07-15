dbec2f4f20d6c48027c82260ee4081b3371923b6
AlexLiang <liangalex91@gmail.com>
2017-07-15 00:56:52 -0400

    Create configure store inside store directory

--- Ashish_P/perview/src/index.js
+++ Ashish_P/perview/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App.js';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
