9be8b79fc7fc9a775dee620bab6aae7d03dfac22
AlexLiang <liangalex91@gmail.com>
2017-07-16 19:35:27 -0400

    Add Nav Bar component to App.js

--- Ashish_P/perview/src/containers/App.js
+++ Ashish_P/perview/src/containers/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import SignInContainer from './SignInContainer';
 import HeaderContainer from './HeaderContainer';
-
+import NavBar from '../render_components/NavBar/NavBar';
 
 const App = ({ children }) => {
   let toRender = null;
   if(!this.props){
     toRender = (
-      <HeaderContainer />
+      <div>
+        <HeaderContainer />
+        <NavBar />
+      </div>
     );
   } else {
     toRender = (
---
+++ Ashish_P/perview/src/render_components/NavBar/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+const NavBar = ({ pageTitle = "Perviews", canCreatePerviews = true }) => {
+  const renderCreateButton = () => {
+    if (canCreatePerviews) {
+      return (
+        <button className="nav-bar__create-btn">Create perviews</button>
+      )
+    }
+  }
+
+  return (
+    <div className="nav-bar__container">
+      <div className="nav-bar__title">
+        {pageTitle}
+      </div>
+      { renderCreateButton() }
+    </div>
+  )
+}
+
+export default withRouter(NavBar);
