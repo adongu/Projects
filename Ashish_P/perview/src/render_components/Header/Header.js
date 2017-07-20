c50620dd01f0fb3fd3c315f108afb1b99158713d
AlexLiang <liangalex91@gmail.com>
2017-07-19 21:16:08 -0400

    Add css for header logo

--- Ashish_P/perview/src/render_components/Header/Header.js
+++ Ashish_P/perview/src/render_components/Header/Header.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import SearchBar from './SearchBar/SearchBar'
+import "../../styles/stylesheets/header.css";
 
 const Header = ({currentUser = "Susan"}) => {
   return (
     <div className="header__container">
-      <div className="header__logo">
-        <img className="header__logo-img" width="40px" src="https://s-media-cache-ak0.pinimg.com/originals/0d/a4/83/0da483f1a56d7c5ba8df57d156c4a371.jpg"/>
-      </div>
-      <div className="header__searchBar">
-        <SearchBar />
-      </div>
-      <div className="header__greetings">
-        Hello, { currentUser }!
-      </div>
-      <div >
+      <div className="row header__box">
+        <div className="header__logo">
+          <img className="header__logoimg" width="40px" src="https://s-media-cache-ak0.pinimg.com/originals/0d/a4/83/0da483f1a56d7c5ba8df57d156c4a371.jpg"/>
+        </div>
+        <div className="header__searchbar">
+          <SearchBar />
+        </div>
+        <div className="header__usernav">
+          <div className="header__greetings">
+            Hello, { currentUser }!
+          </div>
+          <div className="header__usernavphoto">
+          </div>
+        </div>
       </div>
     </div>
   )
