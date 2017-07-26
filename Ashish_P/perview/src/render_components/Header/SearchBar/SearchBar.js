705dd124aaffa3a073d8f65d07ff42410ef91ef8
AlexLiang <liangalex91@gmail.com>
2017-07-26 00:35:14 -0400

    Add styling to autosuggest

--- Ashish_P/perview/src/render_components/Header/SearchBar/SearchBar.js
+++ Ashish_P/perview/src/render_components/Header/SearchBar/SearchBar.js
@@ -1,20 +1,56 @@
 import "../../../styles/stylesheets/search.css";
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import Select from 'react-select';
+import Autosuggest from 'react-autosuggest';
+
+const languages = [{ name: 'C', year: 1972 },
+{ name: 'Elm', year: 2012 }];
+
+// Teach Autosuggest how to calculate suggestions for any given input value.
+const getSuggestions = value => {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+
+  return inputLength === 0 ? [] : languages.filter(lang =>
+    lang.name.toLowerCase().slice(0, inputLength) === inputValue
+  );
+};
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion.name;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+  <div>
+    {suggestion.name}
+  </div>
+);
+
+const renderInputComponent = inputProps => (
+  <div className="search__inputContainer">
+    <input {...inputProps} />
+    <button className="search__btn">
+      <i className="fa fa-search search__btn-icon" aria-hidden="true"></i>
+    </button>
+  </div>
+);
 
 class SearchBar extends React.Component {
   constructor(props){
     super(props);
     this.state = {
       keywords: "",
-      value: ""
+      value: '',
+      suggestions: []
      };
 
     this.renderResults = this.renderResults.bind(this);
-    this.renderSuggestions = this.renderSuggestions.bind(this);
-    this.updateKeywords = this.updateKeywords.bind(this);
+    // this.updateKeywords = this.updateKeywords.bind(this);
     this.logChange = this.logChange.bind(this);
+    this.renderSuggestions = this.renderSuggestions.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
   renderResults({img, title, price, perviews}) {
@@ -32,15 +68,35 @@ class SearchBar extends React.Component {
     console.log("Selected: " + JSON.stringify(val));
   }
 
-  renderSuggestions(searchResults = [{img: "img", title: "title", price: 149, perviews: 2}]) {
-    console.log(this.state.keywords.length);
-    if (this.state.keywords.length > 0) {
-      let maxNumResults = Math.min(searchResults.length, 3)
-      for(let i = 0; i < maxNumResults; i++) {
-        this.renderResults(searchResults[i])
-      }
-    }
-  }
+  // Use your imagination to render suggestions.
+  renderSuggestions(suggestion) {
+    return (
+      <div>
+        {suggestion.name}
+      </div>
+    )
+  };
+
+  onChange (event, { newValue }) {
+    this.setState({
+      value: newValue
+    });
+  };
+
+  // Autosuggest will call this function every time you need to update suggestions.
+  // You already implemented this logic above, so just use it.
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: getSuggestions(value)
+    });
+  };
+
+  // Autosuggest will call this function every time you need to clear suggestions.
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
 
   updateKeywords(e) {
     this.setState({
@@ -59,38 +115,33 @@ class SearchBar extends React.Component {
     this.setState({value: val.value});
   }
 
-  arrowRenderer () {
-  	return (
-  		<i className="fa fa-search search__btn-icon" aria-hidden="true"></i>
-  	);
-  }
-
   render() {
-    let options = [
-      { value: 'one', label: 'One', className: 'search__options-one'},
-      { value: 'two', label: 'Two',  className: 'search__options-two'}
-    ]
+
+    const { value, suggestions } = this.state;
+
+    // Autosuggest will pass through all these props to the input.
+    const inputProps = {
+      placeholder: 'Search on Amazon',
+      value,
+      onChange: this.onChange,
+      type: 'search'
+    };
 
     return (
-      <div>
       <div className="search__container">
         <div className="search__box">
-          <form className="flexrow search__bar">
-            <input onInput={ this.updateKeywords } className="search__input" placeholder="Search on amazon"></input>
-            <button className="search__btn">
-              <i className="fa fa-search search__btn-icon" aria-hidden="true"></i></button>
-          </form>
+          <Autosuggest
+            id="header__suggest"
+            suggestions={suggestions}
+            renderInputComponent
+            onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+            getSuggestionValue={getSuggestionValue}
+            renderSuggestion={renderSuggestion}
+            renderInputComponent={renderInputComponent}
+            inputProps={inputProps}
+          />
         </div>
-      </div>
-      <Select
-        className="search__autosuggest"
-        name="form-field-name"
-        placeholder="Search on Amazon"
-        arrowRenderer={ this.arrowRenderer}
-        value={this.state.value}
-        menuBuffer={4}
-        options={options}
-      />
     </div>
     )
   }
