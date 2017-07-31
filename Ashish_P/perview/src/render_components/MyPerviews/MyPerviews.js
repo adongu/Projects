f677683e1c9c7ed658fee3512bbce625beccc89f
AlexLiang <liangalex91@gmail.com>
2017-07-31 19:56:08 -0400

    Update myperviews page

--- Ashish_P/perview/src/render_components/MyPerviews/MyPerviews.js
+++ Ashish_P/perview/src/render_components/MyPerviews/MyPerviews.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import WidePerview from "../PerviewsLayouts/WidePerview";
+// import WidePerview from "../PerviewsLayouts/WidePerview";
 // switch to PersonalPerview when the component is finish building
-import PersonalPerview from "../PerviewsLayouts/PersonalPerview";
+import NarrowPerview from "../PerviewsLayouts/NarrowPerview";
 
 class MyPerviews extends React.Component {
   constructor (props) {
@@ -12,17 +12,17 @@ class MyPerviews extends React.Component {
 
   getPerviews () {
     let perviews = [];
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < 3; i++) {
       let product = {
         left: {
           img: 'http://saveabandonedbabies.org/wp-content/uploads/2015/08/default.png',
-          title: 'MyPerviews Page',
-          price: 'price',
-          perviews: 'perviews'
+          title: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+          price: 145,
+          perviews: 8
         },
         right: {
           icon: 'https://www.juicedb.com/img/user/temp-user-128.jpg',
-          name: 'name',
+          name: 'Sam White',
           time: "Tuesday at 3:00 pm",
           rating: 3,
           tags: '#amazing #wow @almostlikeapet',
@@ -37,7 +37,7 @@ class MyPerviews extends React.Component {
 
   render () {
     return (
-      <WidePerview
+      <NarrowPerview
         perviews={this.getPerviews()}
         />
     )
---
+++ Ashish_P/perview/src/render_components/PerviewsLayouts/NarrowPerview.js
@@ -0,0 +1,54 @@
+import "../../styles/stylesheets/WidePerviews.css"
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { Button, ButtonToolbar } from 'react-bootstrap';
+
+const NarrowPerview = ({ perviews }) => {
+
+  const renderPerviews = () => {
+    return perviews.map((perview, i) => {
+      return (
+        <div key={`perviewindex__${i}`} className="flexcolumn narrowperviews__box">
+          <div className="flexcolumn narrowperviews__productbox">
+            <div className="narrowperviews__time">{perview.left.time}</div>
+            <div className="narrowperviews__img"><img className="narrowperviews__productimg-photo" src={perview.left.img}/></div>
+            <div className="narrowperviews__name">{perview.left.title}</div>
+            <div className="narrowperviews__price">${perview.left.price}</div>
+            <div className="flexrow">
+              <ButtonToolbar>
+                 <Button className="narrowperviews__product-buybtn" href="http://google.com">BUY AT AMAZON</Button>
+              </ButtonToolbar>
+              <div className="narrowperviews__product-numperviews">{perview.left.perviews} perviews</div>
+            </div>
+          </div>
+
+          <div className="flexcolumn narrowperviews__review-box">
+            <div className="flexrow narrowperviews__review-nav">
+              <div className="narrowperviews__review-stars">{perview.right.rating}</div>
+              <div className="flexrow narrowperviews__editbox">
+                <div className="narrowperviews__review-edit">
+                  <i className="fa fa-pencil" aria-hidden="true"></i>
+                  <div className="narrowperviews__review-save">Save</div>
+                </div>
+                <div className="narrowperviews__review-delete">
+                  <i className="fa fa-trash" aria-hidden="true"></i>
+                  <div className="narrowperviews__review-share">Share</div>
+                </div>
+              </div>
+            </div>
+            <div className="narrowperviews__review-tags">{perview.right.tags}</div>
+            <div className="narrowperviews__review-text">{perview.right.perview}</div>
+          </div>
+        </div>
+      )
+    });
+  }
+
+  return (
+    <div className="narrowperviews__container">
+      { renderPerviews() }
+    </div>
+  )
+}
+
+export default withRouter(NarrowPerview);
