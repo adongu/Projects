5f46f30809f2d46004e21177c0967be1a3807d1d
AlexLiang <liangalex91@gmail.com>
2017-08-12 00:34:44 -0400

    Add css to toggle social button

---
+++ Ashish_P/perview/src/containers/ItemPerviewsContainer.js
@@ -0,0 +1,34 @@
+// import { connect } from 'react-redux';
+// import HomePage from "../render_components/Homepage/HomePage";
+// import { fetchUser } from '../actions/session_actions';
+// import { fetchResults } from '../actions/search_item_actions';
+// import { createPerview, fetchAllPerviews, fetchCategoryIds, clearErrors } from '../actions/perview_actions';
+// import {  } from '../actions/social_actions';
+//
+// const mapStateToProps = ({ session, perview, findItem }, ownProps) => {
+//   console.log(perview.allPerviews);
+//   return {
+//     currentUser: session.currentUser,
+//     isFetching: session.isFetching,
+//     requestLoading: perview.requestLoading,
+//     results: findItem.itemResults,
+//     allPerviews: perview.allPerviews,
+//     categoryIds: perview.categoryIds
+//   }
+// };
+//
+// const mapDispatchToProps = (dispatch, ownProps) => {
+//   return {
+//     fetchUser: () => dispatch(fetchUser()),
+//     createPerview: (formData) => dispatch(createPerview(formData)),
+//     fetchResults: (keywords) => dispatch(fetchResults(keywords)),
+//     fetchAllPerviews: (categoryId) => dispatch(fetchAllPerviews(categoryId)),
+//     fetchCategoryIds: (categoryId) => dispatch(fetchCategoryIds(categoryId)),
+//     clearErrors: () => dispatch(clearErrors())
+//   }
+// }
+//
+// export default connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(HomePage);
