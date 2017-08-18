14dd26628b25bc487c3cb97ee17aed3a3422cd58
AlexLiang <liangalex91@gmail.com>
2017-08-18 14:00:11 -0400

    Add NavBar container props

--- Ashish_P/perview/src/containers/NavBarContainer.js
+++ Ashish_P/perview/src/containers/NavBarContainer.js
@@ -2,14 +2,22 @@ import { connect } from 'react-redux';
 import NavBar from "../render_components/NavBar/NavBar";
 import { fetchUser } from '../actions/session_actions';
 import { fetchResults } from '../actions/search_item_actions';
-import { createPerview, createItem, clearErrors } from '../actions/perview_actions';
+import { createPerview, clearErrors } from '../actions/perview_actions';
 
 const mapStateToProps = ({ session, perview, findItem }) => {
-
+  isFetching: session.isFetching,
+  currentUser: session.currentUser,
+  selectedItem: perview.selectedItem,
+  requestLoading: perview.requestLoading,
+  categoryIds: perview.categoryIds,
+  results: findItem.itemResults
 }
 
 const mapDispatchToProps = (dispatch) => {
-
+  fetchUser: () => dispatch(fetchUser()),
+  fetchResults: (keywords) => dispatch(fetchResults(keywords)),
+  createPerview: (formData) => dispatch(createPerview(formData)),
+  clearErrors: () => dispatch(clearErrors())
 }
 
 export default connect(
