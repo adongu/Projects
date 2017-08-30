5fe61d527fc8a7861fe1faa4dd8a04c88f7a639b
AlexLiang <liangalex91@gmail.com>
2017-08-30 17:49:15 -0400

    Modify axios login request

--- Ashish_P/perview/src/util/session_api_util.js
+++ Ashish_P/perview/src/util/session_api_util.js
@@ -16,46 +16,50 @@ function getCsrfToken() {
     }
     return "";
 }
-
-var config = {
-  headers: {
-  'Access-Control-Allow-Origin': '*',
-  // 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'X-Requested-With': 'XMLHttpRequest'
-  },
-
-  xsrfCookieName: 'XSRF-TOKEN',
-  xsrfHeaderName: 'X-XSRF-TOKEN'
-};
+//
+// var config = {
+//   headers: {
+//   'Access-Control-Allow-Origin': '*',
+//   // 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+//     'Content-Type': 'application/x-www-form-urlencoded',
+//     'X-Requested-With': 'XMLHttpRequest'
+//   },
+//
+//   xsrfCookieName: 'XSRF-TOKEN',
+//   xsrfHeaderName: 'X-XSRF-TOKEN'
+// };
 
 export const logIn = () => {
   let token = getCsrfToken();
 
-  return axios.post('/connect/facebook', {
+  return axios({
+    method: 'POST',
+    url: '/connect/facebook',
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Origin': 'http://localhost:8080',
       // 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
       'Accept': 'application/json',
       'Content-Type': 'application/x-www-form-urlencoded',
       'X-Requested-With': 'XMLHttpRequest',
+      'X-CSRF-TOKEN': token
       // 'Origin': '*'
     },
     data: {
       scope: 'user_friends',
     },
-    origin: '*',
-    xsrfCookieName: 'XSRF-TOKEN',
-    xsrfHeaderName: 'X-XSRF-TOKEN'
+    // origin: '*',
+    // xsrfCookieName: 'XSRF-TOKEN',
+    // xsrfHeaderName: 'X-XSRF-TOKEN'
   })
 
-  // return fetch('/connect/facebook', {
+  // return fetch('http://localhost:8080/connect/facebook', {
   //   method: 'POST',
   //   headers: {
   //     'X-Requested-With': 'XMLHttpRequest',
   //     'Access-Control-Allow-Origin': '*',
   //     'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
   //     'X-XSRF-Token': token,
+  //     'X-CSRF-Token': token,
   //     'Content-Type': 'application/x-www-form-urlencoded',
   //     'Accept': 'application/json',
   //     'Origin': '*'
@@ -67,7 +71,7 @@ export const logIn = () => {
 }
 
 export const fetchUser = () => {
-  return axios('/api/user', config)
+  return axios('/api/user')
 }
 
 export const fetchToken = () => {
