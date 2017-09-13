6b83a9db372154d45341977940b0df5c51b332af
AlexLiang <liangalex91@gmail.com>
2017-09-13 01:00:47 -0400

    Add favicon and adjust header dropdown font color

--- Ashish_P/perview/src/render_components/Header/UserNav.js
+++ Ashish_P/perview/src/render_components/Header/UserNav.js
@@ -42,15 +42,15 @@ class UserNav extends React.PureComponent {
           noCaret pullRight id="dropdown-pull-right" className="usernav__dropdown">
           <MenuItem className="usernav__option" eventKey="1">
             <div onClick={this.redirectToSettings}
-              className="usernav__option" >
-              <i className="fa fa-cog" aria-hidden="true"></i>
-              <span>Settings</span>
+              className="usernav__settings" >
+              <i className="fa fa-cog usernav__option-icon" aria-hidden="true"></i>
+              <span className="usernav__option-text">Settings</span>
             </div>
           </MenuItem>
           <MenuItem eventKey="4" className="usernav__option" eventKey="2">
-            <div onClick={this.handleLogout} className="usernav__signout-btn">
-              <i className="fa fa-sign-out" aria-hidden="true"></i>
-              <span>Sign Out</span>
+            <div onClick={this.handleLogout} className="usernav__signout">
+              <i className="fa fa-sign-out usernav__option-icon" aria-hidden="true"></i>
+              <span className="usernav__option-text">Sign Out</span>
             </div>
           </MenuItem>
 
