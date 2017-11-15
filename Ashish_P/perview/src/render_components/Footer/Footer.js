2c9dfc29b4fbf90b0c0a630e592d1eebe590b760
AlexLiang <liangalex91@gmail.com>
2017-11-14 21:31:49 -0500

    Add amazon disclaimer

--- Ashish_P/perview/src/render_components/Footer/Footer.js
+++ Ashish_P/perview/src/render_components/Footer/Footer.js
@@ -3,11 +3,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
+  const disclaimer = "Disclaimer: CERTAIN CONTENT THAT APPEARS ON THIS SITE, COMES FROM AMAZON SERVICES LLC. THIS CONTENT IS PROVIDED ‘AS IS’ AND IS SUBJECT TO CHANGE OR REMOVAL AT ANY TIME.";
+
   return (
     <div className="footer__container">
       <div className="flexrow footer__box">
-        <span className="footer__item">© Alphathesis LLC. 2017</span>
+        <span>
+          {disclaimer}
+        </span>
 
+        <span className="footer__item">
+          © Alphathesis LLC. 2017
+        </span>
         <Link to="/contact" className="footer__item">Contact Us</Link>
       </div>
     </div>
